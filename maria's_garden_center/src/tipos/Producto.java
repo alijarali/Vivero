09cf02c9a74e094e3f981b0fcdb9cd0e6428f31e
Alicia <Alicia@DESKTOP-4U2S7G9>
2019-05-27 12:30:12 +0200

    Nuevo paquete tipos, con el tipo Producto

---
+++ maria's_garden_center/src/tipos/Producto.java
@@ -0,0 +1,46 @@
+package tipos;
+
+/**
+ * 
+ * Esta es la clase principal, de aquí diferenciaremos el resto de productos
+ */
+public class Producto {
+private String codigo;
+
+/**
+ * Constructor de Producto
+ */
+public Producto(String codigo) {
+				this.codigo = codigo;
+	}
+
+/**
+ * Método Get del código del producto
+ * @return
+ */
+public String getCodigo() {
+	return codigo;
+}
+
+/**
+ * Método Set del código del producto
+ * @param codigo
+ */
+public void setCodigo(String codigo) {
+	this.codigo = codigo;
+}
+
+/**
+ * toString, nos enseña por pantalla de manera "bonita" los atributos del producto.
+ */
+@Override
+public String toString() {
+	return "Producto [Código=" + codigo + "]";
+}
+
+
+
+
+
+
+}
