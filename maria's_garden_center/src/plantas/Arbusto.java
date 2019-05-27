00bb18d227b667c9e80c6221c18055c46c5a0fc6
Alicia <Alicia@DESKTOP-4U2S7G9>
2019-05-27 14:18:47 +0200

    Añadido: clase arbusto clase producto enumeracion TpAbono

--- maria's_garden_center/src/plantas/Arbusto.java
+++ maria's_garden_center/src/plantas/Arbusto.java
@@ -1,5 +1,58 @@
 package plantas;
 
 public class Arbusto {
+	private String colorFlor;
+	private Integer vecesRiegoSemana;
+	
+	/**
+	 * constructor por defecto de arbusto
+	 */
+	public Arbusto() {
+		super();
+		}
+	/**
+	 * Constructor de arbusto
+	 * @param colorFlor
+	 * @param vecesRiegoSemana
+	 */
+	public Arbusto(String colorFlor, Integer vecesRiegoSemana) {
+		super();
+		this.colorFlor = colorFlor;
+		this.vecesRiegoSemana = vecesRiegoSemana;
+	}
+	
+	/**
+	 * método get del atributo ColorFlor
+	 * @return
+	 */
+	public String getColorFlor() {
+		return colorFlor;
+	}
+	
+	/**
+	 * método set del atributo ColorFlor
+	 * @param colorFlor
+	 */
+	public void setColorFlor(String colorFlor) {
+		this.colorFlor = colorFlor;
+	}
+	
+	/**
+	 * método get del atributo vecesRiegoSemana
+	 * @return
+	 */
+	public Integer getVecesRiegoSemana() {
+		return vecesRiegoSemana;
+	}
+	
+/**
+ * método set del atributo vecesRiegoSemana
+ * @param vecesRiegoSemana
+ */
+	public void setVecesRiegoSemana(Integer vecesRiegoSemana) {
+		this.vecesRiegoSemana = vecesRiegoSemana;
+	}
+	
+	
 
 }
--- maria's_garden_center/src/start/App.java
+++ maria's_garden_center/src/start/App.java
@@ -2,11 +2,49 @@ package start;
 
 public class App {
 
+/**
+ * El main nos mostrará el menú
+ * @param args
+ */
 	public static void main(String[] args) {
 
-		// TODO Auto-generated method stub
-		System.out.println("Probando3 - Pedro");
-		System.out.println("Probando4 - Pedro");
+		int opcion = 0;
+		do {
+
+			mostrarMenu();
+			opcion = otros.Utiles.pideDatoEntero("Opción (0-salir): ");
+			ejecutarOpcion(opcion);
+
+		} while (opcion != 0);
+	}
+
+	private static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+		case 1:
+			Opciones.aniadirElemento();;
+			break;
+		case 2:
+			Opciones.borrarElemento();;
+			break;
+		case 3:
+			Opciones.modificarElemento();
+
+			break;
+		case 4:
+			Opciones.listar();
+			break;
+		}
+
+	}
+
+	private static void mostrarMenu() {
+		System.out.println("\n MENU PRINCIPAL");
+		System.out.println("===============");
+		System.out.println("1. Añadir elemento");
+		System.out.println("2. Borrar elemento");
+		System.out.println("3. Modificar elemento");
+		System.out.println("4. Listar");
+
 	}
 
 }
