93bf2fe017c3cb9d2e5ad4d560c0f69afa7095b9
Alicia <Alicia@DESKTOP-4U2S7G9>
2019-05-27 14:23:45 +0200

    quitado el error

--- maria's_garden_center/src/tipos/TpAbono.java
+++ maria's_garden_center/src/tipos/TpAbono.java
@@ -1,18 +1,9 @@
 package tipos;
 
 public enum TpAbono {
-	NATURAL(1), ARTIFICIAL(2), MIXTO(3);
-	
-	private Integer abono;
-	
-	private TpAbono(Integer abono) {
-		this.abono=abono;
-		}
-	
-	public Integer getAbono() {
-		return abono;
+	NATURAL(), ARTIFICIAL(), MIXTO();
+
 	}
 	
 
 	
-}
