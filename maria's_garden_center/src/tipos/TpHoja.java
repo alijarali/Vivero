56b6f69b4ca109351199a08e0470d92998310d7d
usuario <usuario@AMB08>
2019-05-28 11:05:34 +0200

    Adición comentarios en TpHoja

--- maria's_garden_center/src/tipos/TpHoja.java
+++ maria's_garden_center/src/tipos/TpHoja.java
@@ -3,9 +3,9 @@ package tipos;
 public enum TpHoja {
 	/**
 	 * Enumerador que indica el tipo de hoja:
-	 * 	- Caduca
-	 * 	- Perenne
-	 * 	- Otro
+	 * 	- Caduca.
+	 * 	- Perenne.
+	 * 	- Otro.
 	 */
 	CADUCA(), PERENNE(), OTRO();
 
