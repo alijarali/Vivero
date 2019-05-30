d7db92670ba9e4c776eab93fcd44a252ee2785c9
Alicia <Alicia@DESKTOP-4U2S7G9>
2019-05-30 14:08:00 +0200

    Hecha clase borrar elemento close#19  y cierro tareas Alicia close#4 close#3 close#7 close#8 close#12 close#17

--- maria's_garden_center/src/opciones/BorrarElemento.java
+++ maria's_garden_center/src/opciones/BorrarElemento.java
@@ -1,10 +1,28 @@
 package opciones;
 
+import otros.Producto;
+import otros.Utiles;
+import start.App;
+
 public class BorrarElemento {
 	/**
 	 * este método borra un elemento de la lista de productos
 	 */
 public static void borrarElemento() {
-		
+	for (Producto p : start.App.lstProductos) {
+		System.out.println(start.App.lstProductos.indexOf(p)+p.toString());
+	}
+	Integer elemento = Utiles.pideDatoEntero("¿Qué elemento deseas borrar? ");
+	if(elemento!= null) {
+		String seguro= Utiles.pideDatoCadena("¿Está seguro? S/N ");
+		switch(seguro) {
+		case "s": start.App.lstProductos.remove(elemento) ;
+		System.out.println("Se ha borrado el producto de la lista" );;break;
+		case "S": start.App.lstProductos.remove(elemento);
+		System.out.println("Se ha borrado el producto de la lista");break;
+		case "n":System.out.println("Volvemos al menú principal"); ;break;
+		case "N": System.out.println("Volvemos al menú principal");break;
+		}
+	}
 	}
 }
