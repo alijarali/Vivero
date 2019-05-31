366106042aa04d0adb6d92e7a5433385a7e9ba01
usuario <usuario@DESKTOP-OQTS09I>
2019-05-31 09:15:28 +0200

    cuando la lista esta vacia en listar y borar sale un mensaje

--- maria's_garden_center/src/opciones/BorrarElemento.java
+++ maria's_garden_center/src/opciones/BorrarElemento.java
@@ -1,29 +1,47 @@
 package opciones;
 
-import otros.Producto;
 import otros.Utiles;
 
 /**
- * Esta clase borra elementos de la lista. Primero nos enseñará un listado con sus indices 
- * a partir de ahí elegimos cual eliminar
+ * Esta clase borra elementos de la lista. Primero nos enseñará un listado con
+ * sus indices a partir de ahí elegimos cual eliminar
+ * 
  * @author Alicia
  *
  */
 public class BorrarElemento {
-	
-public static void borrarElemento() {
-	Listar.listar();
-	Integer elemento = Utiles.pideDatoEntero("¿Qué elemento deseas borrar? ");
-	if(elemento!= null) {
-		String seguro= Utiles.pideDatoCadena("¿Está seguro? S/N ");
-		switch(seguro) {
-		case "s": start.App.lstProductos.remove(elemento) ;
-		System.out.println("Se ha borrado el producto de la lista" );;break;
-		case "S": start.App.lstProductos.remove(elemento);
-		System.out.println("Se ha borrado el producto de la lista");break;
-		case "n":System.out.println("Volvemos al menú principal"); ;break;
-		case "N": System.out.println("Volvemos al menú principal");break;
+
+	public static void borrarElemento() {
+		Integer l=start.App.lstProductos.size();
+		if(l==0) {
+	 System.out.println("Lista vacía.Puedes añadir productos mediante la opción 'añadir elemento' o cargar el catálogo con la opción 'cargar lista' ");
+	 }else {
+		
+		Listar.listar();
+		
+
+		Integer elemento = Utiles.pideDatoEntero("¿Qué elemento deseas borrar? ");
+		if (elemento != null) {
+			String seguro = Utiles.pideDatoCadena("¿Está seguro? S/N ");
+			switch (seguro) {
+			case "s":
+				start.App.lstProductos.remove(elemento);
+				System.out.println("Se ha borrado el producto de la lista \n");
+				;
+				break;
+			case "S":
+				start.App.lstProductos.remove(elemento);
+				System.out.println("Se ha borrado el producto de la lista \n");
+				break;
+			case "n":
+				System.out.println("Volvemos al menú principal");
+				;
+				break;
+			case "N":
+				System.out.println("Volvemos al menú principal");
+				break;
+			}
 		}
 	}
-	}
+}
 }
--- maria's_garden_center/src/opciones/Listar.java
+++ maria's_garden_center/src/opciones/Listar.java
@@ -7,9 +7,13 @@ public class Listar {
 	 * Este método muestra la lista de productos.
 	 */
 	public static void listar() {
+		if(start.App.lstProductos.size()!=0) {
 		for (Producto p : start.App.lstProductos) {
 			System.out.println(start.App.lstProductos.indexOf(p)+"  "+p.toString());
 		}
-	}
+	}else {
+		System.out.println("No hay elementos en la lista. Puedes añadirlos mediante la opción 'añadir elemento' o cargar el catálogo con la opción 'cargar lista'.");
+		
+	}}
 
 }
